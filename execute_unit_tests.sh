b5797b29660e14761563b750150007ee442ff128
MooreMachine <8643188+MooreMachine@users.noreply.github.com>
2021-03-22 00:43:21 +0100

    master: Run unit tests.

---
+++ execute_unit_tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+VERSION=$(cat README.md | grep "Version" | awk '{ print $2 }')
+
+if [ -z $VERSION ]
+then
+    echo "Please make sure your README.md file has a Version number."
+    exit 1
+fi
+
+echo "Running new container..."
+
+CONTAINER_ID=$(docker run \
+    --name build_test \
+    --rm \
+    -it \
+    -d \
+    -v $(pwd)/app:/home \
+    mooremachine/bareubuntu:$VERSION)
+
+echo "Building app..."
+
+docker exec \
+    -w '/home' \
+    build_test \
+    sh -c './build_unit_tests.sh && ./build/hellotest'
+
+echo "Stopping container..."
+
+docker container stop -t 0 $CONTAINER_ID
+
